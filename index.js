cf34f793c0a112832a574b5081cb1752c2c036f0
alan-nusameta <alan.hertanto@nusameta.io>
2025-06-12 23:42:58 +0700

    test

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const env = require('dotenv');
 env.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/');
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
