9ce1d319eb63363ce8236629aa500dd0b45fa891
alanhertanto <alan.hertanto@gmail.com>
2025-06-12 23:45:57 +0700

    test

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use('/');
-
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
